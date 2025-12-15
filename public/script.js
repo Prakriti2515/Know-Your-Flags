2d202f3539f508f8c8d158961d90fa41115f5070
Prakriti <anukriti.1911@gmail.com>
2025-12-16 01:54:32 +0530

    Country name converted to code

--- public/script.js
+++ public/script.js
@@ -8,18 +8,40 @@ input.addEventListener("keypress", (e)=>{
         getFlag();
     }
 })
+
+const nameToCode = {};
+async function loadMetaData(){
+    try{
+        const res = await fetch("https://restcountries.com/v3.1/all?fields=name,cca2")
+        const data = await res.json()
+
+        data.forEach(country=>{
+            if(country.name?.common && country.cca2){
+                nameToCode[country.name.common.toLowerCase()]= country.cca2;
+            }
+        })
+    }
+    catch(err){
+        console.error("Failed to load metadata:", err);
+    }
+}
+
+loadMetaData();
+
+
 async function getFlag(){
-    const country = input.value.trim();
+    const country = input.value.trim().toLowerCase();
+    const code = nameToCode[country];
     
     flag.innerHTML = ""
     
-    if(!country){
-        flag.innerHTML = "<p>Country name required!</p>"
+    if(!code){
+        flag.innerHTML = "<p>Country not found!</p>"
         return;
     }
 
     try{
-        const res = await fetch(`/api/flag?country=${country}`);
+        const res = await fetch(`/api/flag?country=${code}`);
         console.log("Response status:", res.status)
         const data = await res.json();
         
@@ -35,4 +57,6 @@ async function getFlag(){
         flag.innerHTML = "<p>Something went wrong. Please try again later.</p>"
     }
 }
+
+
     
